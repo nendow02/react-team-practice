4f714b5c1bee177d4a02eb1ec358b58f479b92b9
Daniel Zhu <danielzhu21347@ucla.edu>
2022-05-17 18:24:26 -0700

    Fixing errors

--- src/components/Daniel.tsx
+++ src/components/Daniel.tsx
@@ -2,7 +2,6 @@ import '../styles/daniel.scss';
 import { useState } from 'react';
 import CustomButton from './shared/CustomButton';
 export default function Daniel(): JSX.Element {
-
   const [number, setNumber] = useState(0);
   const [myValue, setMyValue] = useState('');
   const [anotherValue, setAnotherValue] = useState('');
@@ -17,43 +16,50 @@ export default function Daniel(): JSX.Element {
   */
 
   const handleChange = (didChange: boolean) => {
-      if (didChange)
-        return;
-      else
-        return;
+    if (didChange) return;
+    else return;
   };
-
 
   return (
     <div id="daniel">
-      <input placeholder="Type HIDE to hide the button"
-      onChange={(e) => {
-        setMyValue(e.target.value);
-      }} />
-       {myValue !== 'HIDE' ?
-       <CustomButton text={number.toString()}
-       onClick={() => setNumber(number + 1)}/> : null}
+      <input
+        placeholder="Type HIDE to hide the button"
+        onChange={(e) => {
+          setMyValue(e.target.value);
+        }}
+      />
+      {myValue !== 'HIDE' ? (
+        <CustomButton
+          text={number.toString()}
+          onClick={() => setNumber(number + 1)}
+        />
+      ) : null}
 
+      <div id="myCheckbox">
+        <input
+          type="checkbox"
+          onChange={function (e) {
+            handleChange(e.target.checked);
+          }}
+        />
+        I am a TeachLA Member
+      </div>
 
-        <div id="myCheckbox">
-          <input
-          type = "checkbox"
-          onChange = {function (e) {handleChange(e.target.checked);}}/>
-          I am a TeachLA Member</div>
+      <input
+        placeholder="What number to display?"
+        onChange={(e) => {
+          setAnotherValue(e.target.value);
+        }}
+      />
 
-      <input placeholder="What number to display?"
-      onChange={(e) => {
-        setAnotherValue(e.target.value);
-      }} />
-
-        <ol>
-          {anotherValue === 'One' ? <li>One</li> : null}
-          {anotherValue === 'Two' ? <li>Two</li> : null}
-          {anotherValue === 'Three' ?<li>Three</li> : null}
-          {anotherValue === 'One Hundred' ?<li>One Hundred</li> : null}
-          {anotherValue === 'Two Hundred' ?<li>Two Hundred</li> : null}
-          {anotherValue === 'Three Hundred' ?<li>Three Hundred</li> : null}
-        </ol>
+      <ol>
+        {anotherValue === 'One' ? <li>One</li> : null}
+        {anotherValue === 'Two' ? <li>Two</li> : null}
+        {anotherValue === 'Three' ? <li>Three</li> : null}
+        {anotherValue === 'One Hundred' ? <li>One Hundred</li> : null}
+        {anotherValue === 'Two Hundred' ? <li>Two Hundred</li> : null}
+        {anotherValue === 'Three Hundred' ? <li>Three Hundred</li> : null}
+      </ol>
     </div>
   );
 }
