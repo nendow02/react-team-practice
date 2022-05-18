0055903eb4d9821d25a661601d96b2f3cf4a5d4e
Daniel Zhu <danielzhu21347@ucla.edu>
2022-05-17 18:21:45 -0700

    Fixing errors

--- src/components/Daniel.tsx
+++ src/components/Daniel.tsx
@@ -7,49 +7,52 @@ export default function Daniel(): JSX.Element {
   const [myValue, setMyValue] = useState('');
   const [anotherValue, setAnotherValue] = useState('');
 
+  /*
   const shouldDisplay = (theValue: boolean) => {
     if (theValue)
     {
-      return <CustomButton text={number.toString()} onClick={() => setNumber(number + 1)}/>; 
+      return <CustomButton text={number.toString()} onClick={() => setNumber(number + 1)}/>;
     }
-  }
+  };
+  */
 
   const handleChange = (didChange: boolean) => {
       if (didChange)
-        console.log("TeachLA Member");
+        return;
       else
-        console.log("Not a TeachLA Member")
-  }
+        return;
+  };
+
+
   return (
     <div id="daniel">
-      <input placeholder="Type HIDE to hide the button" 
+      <input placeholder="Type HIDE to hide the button"
       onChange={(e) => {
         setMyValue(e.target.value);
-      }} />   
-       {myValue !== 'HIDE' ?  
-       <CustomButton text={number.toString()} 
+      }} />
+       {myValue !== 'HIDE' ?
+       <CustomButton text={number.toString()}
        onClick={() => setNumber(number + 1)}/> : null}
-       
 
 
         <div id="myCheckbox">
-          <input 
+          <input
           type = "checkbox"
-          onChange = {function (e) {handleChange(e.target.checked)}}/>
+          onChange = {function (e) {handleChange(e.target.checked);}}/>
           I am a TeachLA Member</div>
 
       <input placeholder="What number to display?"
       onChange={(e) => {
         setAnotherValue(e.target.value);
-      }} /> 
+      }} />
 
         <ol>
-          {anotherValue === "One" ? <li>One</li> : null} 
-          {anotherValue === "Two" ? <li>Two</li> : null}
-          {anotherValue === "Three" ?<li>Three</li> : null}
-          {anotherValue === "One Hundred" ?<li>One Hundred</li> : null}
-          {anotherValue === "Two Hundred" ?<li>Two Hundred</li> : null}
-          {anotherValue === "Three Hundred" ?<li>Three Hundred</li> : null}
+          {anotherValue === 'One' ? <li>One</li> : null}
+          {anotherValue === 'Two' ? <li>Two</li> : null}
+          {anotherValue === 'Three' ?<li>Three</li> : null}
+          {anotherValue === 'One Hundred' ?<li>One Hundred</li> : null}
+          {anotherValue === 'Two Hundred' ?<li>Two Hundred</li> : null}
+          {anotherValue === 'Three Hundred' ?<li>Three Hundred</li> : null}
         </ol>
     </div>
   );
