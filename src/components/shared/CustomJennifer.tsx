d330b083b1dcb11d953a3f1b59b05bc5481bc5e6
jennifer2430 <jchen243028@gmail.com>
2022-05-17 18:25:04 -0700

    Initial Commit

--- src/components/shared/CustomJennifer.tsx
+++ src/components/shared/CustomJennifer.tsx
@@ -1,15 +1,12 @@
-interface CustomJenniferProp
-{
-    text: string;
-    onClick: ()=>void
+interface CustomJenniferProp {
+  text: string;
+  onClick: () => void;
 }
 
-export default function CustomJennifer(props: CustomJenniferProp) : JSX.Element
-{
-    return (
-        <div>
-            <input type="text" placeholder={props.text} onClick={props.onClick}/>
-        </div>
-    );
-
+export default function CustomJennifer(props: CustomJenniferProp): JSX.Element {
+  return (
+    <div>
+      <input type="text" placeholder={props.text} onClick={props.onClick} />
+    </div>
+  );
 }
