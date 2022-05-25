48a457f08658fae7f5c2b9ef87554b171ad0806d
Aparna Hariharan <aparna@wifi-131-179-0-58.host.ucla.edu>
2022-05-24 18:43:31 -0700

    Changes to my section

--- src/components/AparnaComp.tsx
+++ src/components/AparnaComp.tsx
@@ -1,8 +1,8 @@
-export default function AparnaComponent(): JSX.Element{
-    const greeting = 'Hi!';
-    return(
-        <div id = "welcome-page">
-            <p>{greeting}</p>
-        </div>
-    );
+export default function AparnaComponent(): JSX.Element {
+  const greeting = 'Hi!';
+  return (
+    <div id="welcome-page">
+      <p>{greeting}</p>
+    </div>
+  );
 }
