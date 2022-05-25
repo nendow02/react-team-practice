3ec1e53b36e63fd321b222dcf86294e4bf183196
Aparna Hariharan <aparna@wifi-131-179-0-58.host.ucla.edu>
2022-05-24 18:30:56 -0700

    Changes for my section

--- src/components/Aparna.tsx
+++ src/components/Aparna.tsx
@@ -1,8 +1,32 @@
+import CustomButton from './shared/CustomButton';
+import { useState } from 'react';
+import {AparnaComponent} from 'AparnaComp';
 import '../styles/aparna.scss';
 export default function Aparna(): JSX.Element {
+  function handleChange(a : boolean){
+    if(a){
+      console.log("We're happy you're a part of TeachLA!")
+    };
+  }
+
+  const feelings = ["happy", "sad"];
+
+  const[number, setNumber] = useState(0);
+  const[bool, setBool] = useState(true);
+  
+
   return (
     <div id="aparna">
-      <p>aparna</p>
+      <p>"aparna"</p>
+      <a href = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fwave-hi-sticker-wave-hi-hello-discover-share-gifs--149392912630760632%2F&psig=AOvVaw2uYcjGHywCEhEcJiEUQh7Q&ust=1650505754822000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJCO8ZvDofcCFQAAAAAdAAAAABAJ">"Hello World"</a>
+      <img src = "https://www.thespruce.com/thmb/2fz1zlPNq7cj7QkLAtKdqYrKvs0=/3704x2778/smart/filters:no_upscale()/the-difference-between-trees-and-shrubs-3269804-hero-a4000090f0714f59a8ec6201ad250d90.jpg" alt = "Tree" width = "100" height = "100"/>
+      <CustomButton text = {bool ? "True!" : "False!"} onClick={()=> setBool(!bool)}/> Set to False!
+      <CustomButton text = {number.toString()} onClick={() => setNumber(number+1)}/> Add 1 to number!
+      {number === 5 ? <p>Test</p> : null}  
+      {feelings.map((feeling) => <div key = {feeling}> I'm feeling {feeling}</div>)}
+      <div id="check">
+        <input type = "checkbox" onChange = {function(e){handleChange(e.target.checked)}}/>I am in ACM Teach LA
+      </div>
     </div>
   );
 }
