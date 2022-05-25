e9f45c0843d0bde31f92611d007a6c8d06543317
Cadence Chang <cadencechang@Cadences-MacBook-Air.local>
2022-05-24 18:58:22 -0700

    Cadence

--- src/components/Cadence.tsx
+++ src/components/Cadence.tsx
@@ -1,8 +1,33 @@
 import '../styles/cadence.scss';
+import { useState } from 'react';
+import CustomButton from './shared/CustomButton';
 export default function Cadence(): JSX.Element {
+function handleChange(myBool : boolean)
+{
+  console.log(myBool);
+}
+const [number, setNumber] = useState(0);
+const [my_string, setMy_String] = useState("Type something.");
   return (
     <div id="cadence">
       <p>cadence</p>
+      <p>Hello World</p>
+      <div id = "checkbox">
+      <input
+      type = "checkbox"
+      onChange={function (e) {handleChange(e.target.checked)}}
+      />
+      I am in ACM TeachLA!
+      <CustomButton text = {number.toString()} onClick = {() => setNumber(number + 1) }/>
+      </div>
+      <input
+      type = "text" 
+      onChange = {function (e) {setMy_String(e.target.value)}}
+      />
+      <p>
+        {my_string}
+      </p>
     </div>
+
   );
 }
