18052999463d34ceff52f4334fdb5724edd84c8a
Natalie Lord <natalielordd@gmail.com>
2023-05-02 16:59:52 -0700

    Natalie's section

--- src/components/Section5.tsx
+++ src/components/Section5.tsx
@@ -3,7 +3,14 @@ export default function Section5(): JSX.Element {
   return (
     // You will be coding here!
     <div id="section5">
-      <p>section 5</p>
+      <h1>Natalie</h1>
+      <p>About me:</p>
+      <ol>
+        <li> I'm from Florida. </li>
+        <li> I'm a computer science major.</li>
+        <li> I love matcha! </li>
+      </ol>
+      <img src="https://media.istockphoto.com/id/814013890/vector/matcha-green-tea-latte-cappuccino-drink-top-view.jpg?s=612x612&w=0&k=20&c=0nAGHAduZGIYQqGaDJmbZRfIKwz6BpyCFCo4sKOgJT0=" height="200" width="200"/>
     </div>
   );
 }
