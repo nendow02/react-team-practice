b8a197775e9efc71f13797a8a66bbfb00376a3f7
Natalie Lord <natalielordd@gmail.com>
2023-05-15 11:22:08 -0700

    Section update

--- src/components/Section5.tsx
+++ src/components/Section5.tsx
@@ -9,6 +9,7 @@ export default function Section5(): JSX.Element {
         <li> I'm from Florida. </li>
         <li> I'm a computer science major.</li>
         <li> I love matcha! </li>
+        <li> I have run a half marathon</li>
       </ol>
       <img src="https://media.istockphoto.com/id/814013890/vector/matcha-green-tea-latte-cappuccino-drink-top-view.jpg?s=612x612&w=0&k=20&c=0nAGHAduZGIYQqGaDJmbZRfIKwz6BpyCFCo4sKOgJT0=" height="200" width="200"/>
     </div>
